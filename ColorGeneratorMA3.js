07a80e114c44049f78ed2268a4e062e5425cad7c
«Simen <simen.samuelsen@gmail.com>
2024-07-18 18:51:02 +0200

    updated so that all sequence uses one recipie line for each group specified instead og creating an "allgroup"

--- ColorGeneratorMA3.js
+++ ColorGeneratorMA3.js
@@ -4,6 +4,7 @@
 
 const setup =
 {
+    colorPresetStart: 1,
     numberOfColors: 25,
     filledImageStart: 1,
     unfilledImageStart: 26,
@@ -21,84 +22,124 @@ const setup =
  * Don't change anything below if you don't know what you are doing
  *******************************************/
 
-const fs = require('fs');
-const crypto = require('crypto');
+const fs = require('fs')
+const crypto = require('crypto')
 
 function write(text)
 {
     fs.writeFile('./MA3-ColorPicker-Macros.xml', text + "\n", err => {
         if (err) {
-          console.error(err);
+          console.error(err)
         }
-      });
+      })
 }
 
 function macroLine(command, name = 'macro') {
-    return '<MacroLine Guid="'+crypto.randomUUID()+'"Name="'+ name +'" Command="'+command+'"/>';
+    return '<MacroLine Guid="'+crypto.randomUUID()+'"Name="'+ name +'" Command="'+command+'"/>'
 }
 
+/*******************************************
+ * Generator function
+ *******************************************/
 function generate()
 {
     // Fetch from setup object
-    let numberOfColors = setup.numberOfColors;
-    let filledImageStart = setup.filledImageStart;
-    let unfilledImageStart = setup.unfilledImageStart;
-    let appearanceStart = setup.appearanceStart;
 
-    let groups = ['All'].concat(setup.groups)
+    let colorPresetStart = setup.colorPresetStart
+    let numberOfColors = setup.numberOfColors
+    let filledImageStart = setup.filledImageStart
+    let unfilledImageStart = setup.unfilledImageStart
+    let appearanceStart = setup.appearanceStart
+
     // End fetch from setup object
 
+    // setup variables
     let imagePoolRefrenceForApperanceAssignment = "13.2.3."
+    let allSequenceName = 'All'
     let seqnamePostfix = "Colors"
+    var xml = ''
 
-    var xml = '<?xml version="1.0" encoding="UTF-8"?><GMA3 DataVersion="1.9.7.0">';
-
-    // macro 1 - variabel, sequence, appearance, preset, layout and group creation
-    xml += '<Macro name="Setup ColorPicker" Guid="'+crypto.randomUUID()+'">';
-    xml += macroLine('ClearAll')
+    /*******************************************
+    * Create setup macro
+    *******************************************/
+    xml += '<Macro name="Setup ColorPicker" Guid="'+crypto.randomUUID()+'">'
+    xml += macroLine('ClearAll', 'ClearAll')
 
     if (setup.autoCreateColors)
     {
-        xml += macroLine('AutoCreate Universal 1 At Preset 4.\'*\' \'AmountHue\' ' + setup.autoCreateColors.AmountHue + ' \'AmountSaturation\' '+ setup.autoCreateColors.AmountSaturation +' \'SortColor\' \'Hue\' /NoConfirmation', 'Auto create color presets')
+        xml += macroLine('AutoCreate Universal 1 At Preset 4.' + colorPresetStart + ' \'AmountHue\' ' + setup.autoCreateColors.AmountHue + ' \'AmountSaturation\' '+ setup.autoCreateColors.AmountSaturation +' \'SortColor\' \'Hue\' /NoConfirmation', 'Auto create color presets')
     }
 
-    groups.map((key) => {
-        let seqName = key + seqnamePostfix
-        xml += macroLine('Store Group \'' + key + '\' /m', 'create color groups')
-        xml += macroLine('Store Seq \'' + seqName + '\'', 'Create Sequence for all colors')
-        xml += macroLine('Set seq \''+ seqName + '\' Property \'offwhenoverridden\' false', 'Do not off sequenc when overridden')
-        xml += macroLine('Store Cue 1 Thru ' + numberOfColors + ' Seq \'' + seqName + '\' /o', 'Create cue for each number of colors')
-        xml += macroLine('Assign Group \'' + key + '\' at seq \'' + seqName + '\' cue 1 thru part 0.1')
+    setup.groups.map((key) => {
+        xml += macroLine('Store Group \'' + key + '\' /m', 'create color group if it is missing')
     })
 
+    /*******************************************
+     * Create layout named "Colors".
+     *******************************************/
     xml += macroLine('Store Layout \'Colors\'', 'Create layout for colors')
 
+    /*******************************************
+     * Create an "All" sequence with each group
+     * as an invidual recipie line in each group.
+     *******************************************/
+    let seqName = allSequenceName + seqnamePostfix
+    xml += macroLine('Store Seq \'' + seqName + '\'', 'Create Sequence for all colors')
+    xml += macroLine('Set seq \''+ seqName + '\' Property \'offwhenoverridden\' false', 'Do not off sequenc when overridden')
+    xml += macroLine('Store Cue 1 Thru ' + numberOfColors + ' Seq \'' + seqName + '\' /o', 'Create cue for each number of colors')
+
+    setup.groups.map((key, index) => {
+        xml += macroLine('Assign Group \'' + key + '\' at seq \'' + seqName + '\' cue 1 thru part 0.' + (index + 1 ), 'Assign each group to seperate recipie line inn \'' + allSequenceName + '\' sequence')
+    })
+
+    setup.groups.map((key) => {
+        let seqName = key + seqnamePostfix
+        xml += macroLine('Store Seq \'' + seqName + '\'', 'Create Sequence for group ', key)
+        xml += macroLine('Set seq \''+ seqName + '\' Property \'offwhenoverridden\' false', 'Do not off sequence when overridden')
+        xml += macroLine('Store Cue 1 Thru ' + numberOfColors + ' Seq \'' + seqName + '\' /o', 'Create cue for each number of colors')
+        xml += macroLine('Assign Group \'' + key + '\' at seq \'' + seqName + '\' cue 1 thru part 0.1', 'Assign group at preset line 1 for each cue')
+    })
+
+    /*******************************************
+     * Assign color preset to recipie line in
+     * each color sequence.
+     *******************************************/
+
+    // add "all" to the list of groups
+    let groups = [allSequenceName].concat(setup.groups)
+
+    // for number of colors assign preset number to recipieline
     for (let j = 0; j < numberOfColors; j++)
     {
         xml += macroLine('Setu A' + j + ' \'' + groups.map( function(_, index){ return ((index * numberOfColors) + appearanceStart + j) }).join(" + ") + '\'', 'Appearance colorvariable A'+j)
+        xml += macroLine('Store Appearance $A' + j, 'Creat apperance for colorvariable A'+j)
+        xml += macroLine('Set Appearance $A' + j + ' Property Appearance \'' + imagePoolRefrenceForApperanceAssignment + (j + unfilledImageStart) + '\'', 'Set appearance for colorvariable A'+j)
+
         groups.map((key, index) => {
             let macroName = key + ' ' + (j + 1)
             let seqName = key + seqnamePostfix
 
-            xml += macroLine('Store Appearance $A' + j, 'Creat apperance for colorvariable A'+j)
-            xml += macroLine('Set Appearance $A' + j + ' Property Appearance \'' + imagePoolRefrenceForApperanceAssignment + (j + unfilledImageStart) + '\'', 'Set appearance for colorvariable A'+j)
-            xml += macroLine('Assign Appearance ' + ((index * numberOfColors) + appearanceStart + j) + ' At Macro \'' + macroName + '\'', 'Assign appearance to macro: ' + macroName)
-            xml += macroLine('Assign Preset 4.' + (j+1) + ' At Seq \'' + seqName + '\' cue ' + (j+1) + ' part 0.1', 'Assign Color preset 1 to n at each cue')
+            xml += macroLine('Assign Appearance ' + ((index * numberOfColors) + appearanceStart + j) + ' At Macro \'' + macroName + '\'', 'Assign appearance to macro ' + macroName)
+            xml += macroLine('Assign Preset 4.' + (j+colorPresetStart) + ' At Seq \'' + seqName + '\' cue ' + (j+1) + ' part 0.1 thru', 'Assign Color preset 1 to n at each cue')
 
             // Assign to layout
-            xml += macroLine('Assign Macro \'' + macroName + '\' at Layout \'Colors\'')
-            xml += macroLine('Set Layout \'Colors\'. \'' + macroName + '\' \'posX\' \'' + (50 * j) + '\'')
-            xml += macroLine('Set Layout \'Colors\'.\'' + macroName + '\' \'posY\' \'' + (-50 * index) + '\'')
+            xml += macroLine('Assign Macro \'' + macroName + '\' at Layout \'Colors\'', 'Add Macro to layoutview')
+            xml += macroLine('Set Layout \'Colors\'. \'' + macroName + '\' \'posX\' \'' + (50 * j) + '\'', 'Set X-position for macro')
+            xml += macroLine('Set Layout \'Colors\'.\'' + macroName + '\' \'posY\' \'' + (-50 * index) + '\'', 'Set Y-postion for macro')
         })
     }
-    xml += '</Macro>';
+    xml += '</Macro>'
 
-    // macro 2 - Allcolor macros
+    /*******************************************
+     * Create macros to trigger color cues
+     *******************************************/
+
+    // Macros for "All" sequence
     groups.filter((_, index) => index === 0).map((key) => {
         let seqName = key + seqnamePostfix
         for(let j = 0; j < numberOfColors; j++) {
             let macroName = key + ' ' + (j + 1)
-            xml += '<Macro name="' + macroName + '" Guid="'+crypto.randomUUID()+'">';
+            xml += '<Macro name="' + macroName + '" Guid="'+crypto.randomUUID()+'">'
             let setUnfilled = ''
             for (k = 0; k < numberOfColors; k++){
                 setUnfilled += 'Set Appearance $A' + k + ' Property Appearance \'' + imagePoolRefrenceForApperanceAssignment + (k + unfilledImageStart) + '\';'
@@ -110,14 +151,14 @@ function generate()
         }
     })
 
-    // macro 3 - Groupcolor macros
+    // Macros for each group
     groups.filter((_, index) => index > 0).map((key, index) => {
         let seqName = key + seqnamePostfix
-        let appearancePoolItem = ((index+1) * numberOfColors) + appearanceStart;
+        let appearancePoolItem = ((index+1) * numberOfColors) + appearanceStart
 
         for(let j = 0; j < numberOfColors; j++) {
             let macroName = key + ' ' + (j + 1)
-            xml += '<Macro name="' + macroName + '" Guid="'+crypto.randomUUID()+'">';
+            xml += '<Macro name="' + macroName + '" Guid="'+crypto.randomUUID()+'">'
             let setUnfilled = ''
             for (k = 0; k < numberOfColors; k++){
                 // set all colors unfilled
@@ -134,8 +175,8 @@ function generate()
         }
     })
 
-    ///// write everything
-    write(xml + "</GMA3>");
+    // write everything
+    write('<?xml version="1.0" encoding="UTF-8"?><GMA3 DataVersion="1.9.7.0">' + xml + "</GMA3>")
 }
 
 generate()
